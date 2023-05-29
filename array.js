f495cdcb68d1177c9841f542a9fd8f4e132fce97
Ariston <aristoncandido.nw@gmail.com>
2023-05-29 15:56:41 -0300

    Coloquei uma função

--- array.js
+++ array.js
@@ -1,32 +1,44 @@
 
+function Construir(i,j){
+
+   for (i = 0; i < linhas; i++) {
+      m[i] = [];
+    
+      for (j = 0; j < colunas; j++) {
+        if (i == 0) {
+          // primeira linha que é Múltiplo de 2
+    
+          m[i][j] = j * 2;
+        } else if (i == 1) {
+          ///segunda linha Múltiplo de 3
+    
+          m[i][j] = j * 3;
+        } else if (i == 2) {
+          //terceira linha Múltiplo 4
+    
+          m[i][j] = j * 4;
+        } else if (i == 3) {
+          //Quarta linha Múltiplo de 5
+    
+          m[i][j] = j * 5;
+        }
+      }
+    }
+
+
+
+    return m; // retorno da func
+    
+}
+
+
+
 
 var m = [];
 
-linhas = 4;
-colunas = 5;
+var linhas = 4;
+var colunas = 5;
 
-for (i = 0; i < linhas; i++) {
-  m[i] = [];
 
-  for (j = 0; j < colunas; j++) {
-    if (i == 0) {
-      // primeira linha que é Múltiplo de 2
 
-      m[i][j] = j * 2;
-    } else if (i == 1) {
-      ///segunda linha Múltiplo de 3
-
-      m[i][j] = j * 3;
-    } else if (i == 2) {
-      //terceira linha Múltiplo 4
-
-      m[i][j] = j * 4;
-    } else if (i == 3) {
-      //Quarta linha Múltiplo de 5
-
-      m[i][j] = j * 5;
-    }
-  }
-}
-
-console.log(m);
+console.log(Construir(linhas,colunas));
